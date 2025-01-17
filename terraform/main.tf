2dadce726446a66ccd60581a224f8148f5e319c2
ederpbrito <ederpsilva@hotmail.com>
2025-01-16 21:09:42 -0300

    correção do job terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,61 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "techchallangebucket"
+    key            = "hackaton.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 variable "s3_bucket_name" {
   description = "Nome do bucket S3 para armazenar o código Lambda"
   type        = string
   default     = "lambda-code-acompanhamento"
 }
 
-# Criar o bucket S3 com nome fixo
-resource "aws_s3_bucket" "lambda_code_acompanhamento" {
-  bucket = var.s3_bucket_name
+resource "aws_dynamodb_table" "acompanhamento_processo" {
+  name           = "AcompanhamentoProcesso"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id_usuario"
 
-  lifecycle {
-    prevent_destroy = false
+  attribute {
+    name = "id_usuario"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "Production"
+    Project     = "AcompanhamentoProcesso"
   }
 }
 
-# Criar o objeto no S3 para o código da Lambda API
 resource "aws_s3_object" "lambda_api_code" {
-  bucket = aws_s3_bucket.lambda_code_acompanhamento.id
+  bucket = var.s3_bucket_name
   key    = "lambda-api.zip"
   source = "lambda-api.zip"
+
+  provisioner "local-exec" {
+    command = "echo 'Listing directory contents:' && ls -l"
+  }
 }
 
 # Criar o objeto no S3 para o código da Lambda SQS
 resource "aws_s3_object" "lambda_sqs_code" {
-  bucket = aws_s3_bucket.lambda_code_acompanhamento.id
+  bucket = var.s3_bucket_name
   key    = "lambda-sqs.zip"
   source = "lambda-sqs.zip"
+
+  provisioner "local-exec" {
+    command = "echo 'Listing directory contents:' && ls -l"
+  }
 }
 
-# Função Lambda - AcompanhamentoAPI
 resource "aws_lambda_function" "api_lambda" {
   function_name    = "AcompanhamentoAPI"
-  s3_bucket        = aws_s3_bucket.lambda_code_acompanhamento.id
+  s3_bucket        = aws_s3_object.lambda_api_code.bucket
   s3_key           = aws_s3_object.lambda_api_code.key
   handler          = "app.main.handler"
   runtime          = "python3.10"
@@ -44,10 +66,9 @@ resource "aws_lambda_function" "api_lambda" {
   architectures    = ["x86_64"]
 }
 
-# Função Lambda - AcompanhamentoSQS
 resource "aws_lambda_function" "sqs_lambda" {
   function_name    = "AcompanhamentoSQS"
-  s3_bucket        = aws_s3_bucket.lambda_code_acompanhamento.id
+  s3_bucket        = aws_s3_object.lambda_sqs_code.bucket
   s3_key           = aws_s3_object.lambda_sqs_code.key
   handler          = "app.sqs_handler.handler"
   runtime          = "python3.10"
@@ -62,7 +83,6 @@ resource "aws_sqs_queue" "queue_acompanhamento" {
   name = "queue-acompanhamento"
 }
 
-# Permissões para o SQS disparar a Lambda
 resource "aws_lambda_permission" "allow_sqs" {
   statement_id  = "AllowSQSTrigger"
   action        = "lambda:InvokeFunction"
@@ -76,15 +96,22 @@ resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   function_name     = aws_lambda_function.sqs_lambda.arn
   batch_size        = 10
   enabled           = true
+
+  depends_on = [
+    aws_sqs_queue.queue_acompanhamento
+  ]
+
+  lifecycle {
+    ignore_changes = [event_source_arn]
+  }
 }
 
-# Criar o API Gateway
+#Criar o API Gateway
 resource "aws_api_gateway_rest_api" "acompanhamento_api" {
-  name        = "AcompanhamentoAPI"
+  name        = "GatewayAcompanhamento"
   description = "API Gateway para a Lambda AcompanhamentoAPI"
 }
 
-# Criar o recurso do path /api/v1/acompanhamentos/{id}
 resource "aws_api_gateway_resource" "acompanhamentos_resource" {
   rest_api_id = aws_api_gateway_rest_api.acompanhamento_api.id
   parent_id   = aws_api_gateway_rest_api.acompanhamento_api.root_resource_id
@@ -119,7 +146,6 @@ resource "aws_api_gateway_method" "get_acompanhamentos" {
   api_key_required = true
 }
 
-# Configurar a integração com a Lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.acompanhamento_api.id
   resource_id             = aws_api_gateway_resource.id_path.id
@@ -129,7 +155,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.api_lambda.invoke_arn
 }
 
-# Criar a permissão para o API Gateway invocar a Lambda
 resource "aws_lambda_permission" "api_gateway_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -138,20 +163,17 @@ resource "aws_lambda_permission" "api_gateway_permission" {
   source_arn    = "${aws_api_gateway_rest_api.acompanhamento_api.execution_arn}/*/*"
 }
 
-# Implantação da API
 resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.acompanhamento_api.id
 }
 
-# Estágio da API
 resource "aws_api_gateway_stage" "prod_stage" {
   deployment_id = aws_api_gateway_deployment.api_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.acompanhamento_api.id
   stage_name    = "prod"
 }
 
-# Plano de uso
 resource "aws_api_gateway_usage_plan" "usage_plan" {
   name = "AcompanhamentoAPIUsagePlan"
 
@@ -171,13 +193,11 @@ resource "aws_api_gateway_usage_plan" "usage_plan" {
   }
 }
 
-# Criar a chave da API
 resource "aws_api_gateway_api_key" "api_key" {
   name    = "AcompanhamentoAPIKey"
   enabled = true
 }
 
-# Associar a chave da API ao plano de uso
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key" {
   key_id        = aws_api_gateway_api_key.api_key.id
   key_type      = "API_KEY"
@@ -195,10 +215,6 @@ output "data_query_lambda_function_name" {
 
 output "sqs_queue_url" {
   value = aws_sqs_queue.queue_acompanhamento.url
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.lambda_code_acompanhamento.id
 }
 
 output "api_url" {
